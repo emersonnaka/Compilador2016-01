5867ea282a9c93f29c0b82d500801cc6a2ef4a96
emersonnaka <emerson.nakashima@gmail.com>
2016-05-07 01:23:46 -0300

    Semântica completa, porém com erros: arrumar conjParametros(self, node)

--- semantica.py
+++ semantica.py
@@ -81,6 +81,19 @@ class Semantica():
         self.conjParametros(node.children[1])
         self.conjInstrucao(node.children[2])
 
+# def p_tipo(t):
+#     ''' tipo : INTEIRO
+#              | FLUTUANTE
+#              | VAZIO '''
+#     if t[1] == 'inteiro':
+#         t[0] = AST('tipoInteiro', [], t[1])
+#     elif t[1] == 'flutuante':
+#         t[0] = AST('tipoFlutuante', [], [t[1]])
+#     else:
+#         t[0] = AST('tipoVazio', [], [t[1]])
+    def getTipo(self, node):
+        return node.leaf[0]
+
 # def p_conjParametros_(t):
 #     ''' conjParametros : conjParametros VIRGULA tipo DOISPONTOS ID
 #                        | tipo DOISPONTOS ID
@@ -96,35 +109,34 @@ class Semantica():
         if(len(node.children) > 0):
             tipo = self.getTipo(node.children[0])
             variaveis.append(tipo)
-            key = str(self.escopo + '.' + nodel.leaf[0])
-            self.simbolos[key] = ["variável", 0, tipo]
+            self.simbolos[str(self.escopo + '.' + node.leaf[0])] = ['variável', 0, tipo]
+            print(str(self.escopo + '.' + node.leaf[0]))
             if len(node.children) > 1:
-                variaveis = variaveis + conjParametros(node.children[0])
+                print("DENTRO DO IF")
+                print(str(self.escopo + '.' + node.leaf[0]))
+                variaveis = variaveis + self.conjParametros(node.children[0])
+                print("depois das variaveis")
         return variaveis
 
 # def p_declaraVar(t):
 #     ' declaraVar : tipo DOISPONTOS ID NOVALINHA '
 #     t[0] = AST('declaraVar', [t[1]], [t[3]])
     def declaraVar(self, node):
-        tipo = getTipo(node.children[0])
-        key = self.escopo + "." + node.leaf[0]
-        if key is self.simbolos.keys():
+        tipo = self.getTipo(node.children[0])
+        if self.escopo + "." + node.leaf[0] is self.simbolos.keys():
             print("Erro semântico: ID '" + node.leaf[0] + "' já foi declarado")
             exit(1)
         if node.leaf[0] is self.simbolos.keys():
             print("Erro semântico: ID '" + node.leaf[0] + "' foi declarado como função")
             exit(1)
-        self.simbolos[key] = ["variável", 0, tipo]
-
-    def getTipo(self, node):
-        return node.leaf[0]
+        self.simbolos[self.escopo + "." + node.leaf[0]] = ["variável", 0, tipo]
 
 # def p_chamaFuncao(t):
 #     ' chamaFuncao : ID ABREPARENTES parametros FECHAPARENTES '
 #     t[0] = AST('chamaFuncao', [t[3]], [t[1]])
     def chamaFuncao(self, node):
         if node.leaf[0] not in self.simbolos.keys():
-            print("Erro semântico: função '" + node.leaf[0] + "' não foi declarado")
+            print("Erro semântico: função '" + node.leaf[0] + "' não declarada")
             exit(1)
         qtdeParam = self.parametros(node.children[0])
         if len(self.simbolos[node.leaf[0]][2]) != qtdeParam:
@@ -194,15 +206,152 @@ class Semantica():
         if node.children[0].name == 'chamaFuncao':
             self.chamaFuncao(node.children[0])
 
-
+# def p_condicional(t):
+#     ''' condicional : SE conjExpr ENTAO NOVALINHA conjInstrucao FIM NOVALINHA
+#                     | SE conjExpr ENTAO NOVALINHA conjInstrucao SENAO NOVALINHA conjInstrucao FIM NOVALINHA '''
+#     if (len(t) == 8):
+#         t[0] = AST('condicionalSe', [t[2], t[5]])
+#     else:
+#         t[0] = AST('condicionalSenao', [t[2], t[5], t[8]])
     def condicional(self, node):
+        self.conjExpr(node.children[0])
+        self.conjInstrucao(node.children[1])
+        if len(node.children) == 3:
+            self,conjInstrucao(node.children[2])
 
+# def p_repeticao(t):
+#     ' repeticao : REPITA NOVALINHA conjInstrucao ATE conjExpr NOVALINHA'
+#     t[0] = AST('repeticao', [t[3], t[5]])
     def repeticao(self, node):
+        self.conjInstrucao(node.children[0])
+        self.conjExpr(node.children[1])
 
+# def p_atribuicao(t):
+#     ''' atribuicao : ID RECEBE conjExpr NOVALINHA
+#                    | ID RECEBE chamaFuncao NOVALINHA '''
+#     t[0] = AST('atribuicao', [t[3]], [t[1]])
     def atribuicao(self, node):
+        if self.escopo + "." + node.leaf[0] not in self.simbolos.keys():
+            print("Erro semântico: ID '" + node.leaf[0] + "' não declarado")
+            exit(1)
+        if node.children[0].name == 'chamaFuncao':
+            self.chamaFuncao(node.children[0])
+        else:
+            self.conjExpr(node.children[0])
 
+# def p_leitura(t):
+#     ' leitura : LEIA ABREPARENTES ID FECHAPARENTES NOVALINHA '
+#     t[0] = AST('leitura', [], [t[3]])
     def leitura(self, node):
+        if self.escopo + '.' + node.leaf[0] not in self.simbolos.keys():
+            print("Erro semântico: ID '" + node.leaf[0] + "' não declarado")
+            exit(1)
 
+# def p_escreva(t):
+#     ''' escreva : ESCREVA ABREPARENTES conjExpr FECHAPARENTES NOVALINHA
+#                 | ESCREVA ABREPARENTES chamaFuncao FECHAPARENTES NOVALINHA '''
+#     t[0] = AST('escreva', [t[3]])
     def escreva(self, node):
+        if node.children[0].name == 'chamaFuncao':
+            self.chamaFuncao(node.children[0])
+        else:
+            self.conjExpr(node.children[0])
 
+# def p_retorna(t):
+#     ' retorna : RETORNA ABREPARENTES exprArit FECHAPARENTES NOVALINHA '
+#     t[0] = AST('retorna', [t[3]])
     def retorna(self, node):
+        self.exprArit(node.children[0])
+
+# def p_conjExpr(t):
+#     ''' conjExpr : exprArit compara exprArit
+#                  | exprArit '''
+#     if (len(t ) == 4):
+#         t[0] = AST('conjExprComp', [t[1], t[2], t[3]])
+#     else:
+#         t[0] = AST('conjExpr', [t[1]])
+    def conjExpr(self, node):
+        if len(node.children) == 3:
+            self.exprArit(node.children[0])
+            operador = self.compara(node.children[1])
+            self.exprArit(node.children[2])
+        else:
+            self.exprArit(node.children[0])
+
+# def p_compara(t):
+#     ''' compara : MENOR
+#                 | MAIOR
+#                 | MENORIGUAL
+#                 | MAIORIGUAL
+#                 | IGUAL '''
+#     t[0] = AST('compara', [], [t[1]])
+    def compara(self, node):
+        return node.leaf[0]
+
+# def p_exprArit(t):
+#     ''' exprArit : exprArit soma termo 
+#                  | termo '''
+#     if (len(t) == 4):
+#         t[0] = AST('exprAritComp', [t[1], t[2], t[3]])
+#     else:
+#         t[0] = AST('exprArit', [t[1]])
+    def exprArit(self, node):
+        if len(node.children) == 3:
+            self.exprArit(node.children[0])
+            operador = self.soma(node.children[1])
+            self.termo(node.children[2])
+        else:
+            self.termo(node.children[0])
+
+# def p_soma(t):
+#     ''' soma : MAIS
+#              | MENOS '''
+#     t[0] = AST('maisMenos', [], [t[1]])
+    def soma(self, node):
+        return node.leaf[0]
+
+# def p_termo(t):
+#     ''' termo : termo multi fator
+#               | fator '''
+#     if (len(t) == 4):
+#         t[0] = AST('termoComp', [t[1], t[2], t[3]])
+#     else:
+#         t[0] = AST('termo', [t[1]])
+    def termo(self, node):
+        if len(node.children) == 3:
+            self.termo(node.children[0])
+            operador = self.multi(node.children[1])
+            self.fator(node.children[2])
+        else:
+            self.fator(node.children[0])
+
+# def p_multi(t):
+#     ''' multi : VEZES
+#               | DIVIDIR '''
+#     t[0] = AST('vezesDividir', [], [t[1]])
+    def multi(self, node):
+        return node.leaf[0]
+
+# def p_fator_1(t):
+#     ' fator : ABREPARENTES exprArit FECHAPARENTES '
+#     t[0] = AST('fatorExprArti', [t[1]])
+# def p_fator_2(t):
+#     ' fator : num '
+#     t[0] = AST('num', [t[1]])
+# def p_fator_3(t):
+#     ' fator : ID '
+#     t[0] = AST('fatorID', [], [t[1]])
+    def fator(self, node):
+        if node.name == 'fatorID':
+            if self.escopo + '.' + node.leaf[0] not in self.simbolos.keys():
+                print("Erro semântico: ID '" + node.leaf[0] + "' não declarado")
+                exit(1)
+        elif node.name =='fatorExprArit':
+            self.exprArit(node.children[0])
+
+if __name__ == '__main__':
+    import sys
+    code = open(sys.argv[1])
+    s = Semantica(code.read())
+    s.semanticaTopo()
+    print("Tabela de Simbolos:", s.simbolos)
