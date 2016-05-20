b81a230cfbde60e3e4b1f628811c04e63c070836
emersonnaka <emerson.nakashima@gmail.com>
2016-05-19 23:22:48 -0300

    Atributos em português

--- ast.py
+++ ast.py
@@ -9,13 +9,3 @@ class AST():
         for child in self.filho:
             ret += child.__str__(level+1)
         return ret
-
-    def buscaRetorno(self, nó):
-        if nó == None:
-            return False
-        if 'retorna' in nó.nome:
-            return True
-        for i in range(len(nó.filho)):
-            if self.buscaRetorno(nó.filho[i]):
-                return True
-        return False
